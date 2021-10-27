5fc4529ed081e0858b0b4b213c0aaabe01f79656
Vo7ice <Vo7ice@outlook.com>
2021-10-27 17:37:37 +0800

    [C Primer Plus] [Chapter 12]

---
+++ C/Chapter12/global.c
@@ -0,0 +1,28 @@
+/* global.c 使用外部变量 */
+
+#include <stdio.h>
+int units = 0;
+
+void critic(void);
+
+int main(int argc, char const *argv[])
+{
+    extern int units;
+    printf("How many pounds to a firkin of butter? \n");
+    scanf("%d", &units);
+    while (units != 56)
+    {
+        critic();
+    }
+    printf("You must have looked it up!\n");
+    
+    return 0;
+}
+
+void critic()
+{
+    printf("No luck, my friend. Try again.\n");
+    scanf("%d", &units);
+}
+
+
